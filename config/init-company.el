15f2dead0c67036fceb040b19b016d5a934c6211
luhuaei <luhuaei@gmail.com>
2019-06-27 00:36:41 +0800

    add init-lsp and init-flycheck and init-emms modules

--- config/init-company.el
+++ config/init-company.el
@@ -1,5 +1,6 @@
 (use-package company
   :ensure t
   :hook ((emacs-lisp-mode . company-mode)
-	 (ess-r-mode . company-mode)))
+	 (ess-r-mode . company-mode)
+	 (python-mode . company-mode)))
 (provide 'init-company)
