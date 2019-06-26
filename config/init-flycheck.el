abf07a9808898f94f204c939aa8832e5d95633fd
luhuaei <luhuaei@gmail.com>
2019-06-27 00:29:30 +0800

    add flycheck-mode and disable flymake-mode

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -1,1 +1,21 @@
+(use-package flycheck
+  :ensure t
+  :diminish flycheck-mode
+  :hook (after-init . (lambda ()
+			(flymake-mode 0)
+			(global-flycheck-mode t)))
+  :config
+  (setq flycheck-indication-mode 'right-fringe)
+  (setq flycheck-emacs-lisp-load-path 'inherit)
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
+  ;; Only check while saving and opening files
+  (setq flycheck-idle-change-delay 10)
+  (setq flycheck-check-syntax-automatically '(idle-change))
+
+  (use-package flycheck-posframe
+    :ensure t
+    :hook (flycheck-mode . flycheck-posframe-mode)
+    :config (add-to-list 'flycheck-posframe-inhibit-functions
+                         #'(lambda () (bound-and-true-p company-backend)))))
+(provide 'init-flycheck)
