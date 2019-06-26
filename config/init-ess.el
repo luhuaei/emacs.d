73b58a603af10a965cf822e8baa4a2eb4516461e
luhuaei <luhuaei@gmail.com>
2019-06-27 00:37:53 +0800

    add ess buffers displays controls

--- config/init-ess.el
+++ config/init-ess.el
@@ -1,5 +1,23 @@
-
 (use-package ess
-  :ensure t)
+  :ensure t
+  :config
+  (setq ess-ask-for-ess-directory nil)
+  (setq display-buffer-alist
+	`(("*R Dired"
+           (display-buffer-reuse-window display-buffer-in-side-window)
+           (side . right)
+           (slot . -1)
+           (window-width . 0.33)
+           (reusable-frames . nil))
+          ("*R"
+           (display-buffer-reuse-window display-buffer-at-bottom)
+           (window-width . 0.5)
+           (reusable-frames . nil))
+          ("*Help"
+           (display-buffer-reuse-window display-buffer-in-side-window)
+           (side . right)
+           (slot . 1)
+           (window-width . 0.45)
+           (reusable-frames . nil)))))
 
 (provide 'init-ess)
