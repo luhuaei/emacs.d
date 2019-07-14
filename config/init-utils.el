f4fe9fcd486c5db2c05a5605c096329acae69910
luhuaei <luhuaei@gmail.com>
2019-07-14 14:45:04 +0800

    add muliple cursors.

--- config/init-utils.el
+++ config/init-utils.el
@@ -53,4 +53,12 @@
   :config
   (setq highlight-indent-guides-method 'character))
 
+(use-package multiple-cursors
+  :ensure t
+  :config
+  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+
 (provide 'init-utils)
