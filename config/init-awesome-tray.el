c3374263814c053cba48ec2ecfbdce6dc5af42c2
luhuaei <luhuaei@gmail.com>
2019-07-14 14:44:17 +0800

    add display option.

--- config/init-awesome-tray.el
+++ config/init-awesome-tray.el
@@ -3,6 +3,6 @@
   :config
   (awesome-tray-mode 1)
   (setq awesome-tray-active-modules
-	'("last-command" "location" "buffer-name" "parent-dir" "mode-name")))
+	'("last-command"  "location" "buffer-name" "mode-name"  "git" "date")))
 
 (provide 'init-awesome-tray)
