2c48e3c1b43ce6a20c5287d8c36cc65edec2b2b6
luhuaei <luhuaei@gmail.com>
2019-08-16 00:01:27 +0800

    add emacs application frame

---
+++ config/init-eaf.el
@@ -0,0 +1,7 @@
+(use-package eaf
+  :path "~/emacs.d/extensions/emacs-application-framework/"
+  :config
+  (setq eaf-http-proxy-host "127.0.0.1")
+  (setq eaf-http-proxy-port "1080"))
+
+(provide 'init-eaf)
