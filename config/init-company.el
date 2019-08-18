a5aefd15c258a058638375af93f22653a6b35709
luhuaei <luhuaei@gmail.com>
2019-08-18 08:26:30 +0800

    add company-quickhelp

--- config/init-company.el
+++ config/init-company.el
@@ -1,9 +1,44 @@
 (use-package company
   :ensure t
-  :hook ((emacs-lisp-mode . company-mode)
-	 (ess-r-mode . company-mode)
-	 (python-mode . company-mode))
+  :bind (("M-/" . company-complete)
+         ("<backtab>" . company-yasnippet)
+         :map company-active-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next)
+         ("<tab>" . company-complete-common-or-cycle)
+         ("<backtab>" . my-company-yasnippet)
+         ;; ("C-c C-y" . my-company-yasnippet)
+         :map company-search-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next))
+  :hook (after-init . global-company-mode)
   :config
-  (setq company-idle-delay 1)
-  (setq company-candidates-length 3))
+  (setq company-tooltip-align-annotations t ; aligns annotation to the right
+	company-tooltip-limit 24            ; bigger popup window
+	company-idle-delay .3 ; decrease delay before autocompletion popup shows
+	company-echo-delay .2 ; remove annoying blinking
+	company-minimum-prefix-length 2
+	company-require-match nil
+	company-dabbrev-ignore-case nil
+	company-dabbrev-downcase nil)
+  ;; Nicer looking faces
+  (custom-set-faces
+   '(company-tooltip-common
+     ((t (:inherit company-tooltip :weight bold :underline nil))))
+   '(company-tooltip-common-selection
+     ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
+
+  (use-package company-lsp
+    :ensure t
+    :init (setq company-lsp-cache-candidates 'auto))
+  (push 'company-lsp company-backends))
+
+(use-package company-quickhelp
+  :ensure t
+  :defines company-quickhelp-delay
+  :bind (:map company-active-map
+	      ("C-c h" . company-quickhelp-manual-begin))
+  :hook (global-company-mode . company-quickhelp-mode)
+  :init (setq company-quickhelp-delay 0.5))
+
 (provide 'init-company)
