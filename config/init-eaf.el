ff125f8fc7027ede1116e4b490b6b5458e14dbdb
luhuaei <luhuaei@gmail.com>
2019-08-18 08:26:54 +0800

    modified ensure

--- config/init-eaf.el
+++ config/init-eaf.el
@@ -1,5 +1,5 @@
 (use-package eaf
-  :path "~/emacs.d/extensions/emacs-application-framework/"
+  :ensure nil
   :config
   (setq eaf-http-proxy-host "127.0.0.1")
   (setq eaf-http-proxy-port "1080"))
