d3781e01c94ca0fdf50270b617961acd36192a8a
luhuaei <luhuaei@gmail.com>
2019-09-23 17:34:59 +0800

    add emacs-jupyter package, delete lpy mode

--- config/init-python.el
+++ config/init-python.el
@@ -2,7 +2,7 @@
   :ensure nil
   :hook (python-mode . awesome-pair-mode)
   :config
-  ;; (setq python-shell-completion-native-disabled-interpreters '("pypy" "ipython" "python" "ipython3"))
+  (setq python-shell-completion-native-disabled-interpreters '("pypy" "ipython" "python" "ipython3"))
   (setq python-shell-interpreter "/home/luhuaei/.local/bin/ipython3"
 	python-shell-interpreter-args "-i --pylab --simple-prompt --no-color-info")
 
@@ -13,7 +13,8 @@
     (setq
      pipenv-projectile-after-switch-function
      #'pipenv-projectile-after-switch-extended))
-  (use-package lpy
-    :ensure nil))
+
+  (use-package jupyter
+    :ensure t))
 
 (provide 'init-python)
