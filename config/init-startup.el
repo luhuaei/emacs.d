c53a833d19d4bd9872277300a71c0a045b552552
luhuaei <luhuaei@gmail.com>
2019-10-19 22:34:04 +0800

    update

--- config/init-startup.el
+++ config/init-startup.el
@@ -2,7 +2,7 @@
 (menu-bar-mode -1)                      ;禁用菜单栏
 (scroll-bar-mode -1)                    ;禁用滚动条
 (setq inhibit-startup-screen t)		;初始界面
-(toggle-frame-fullscreen)
+;; (toggle-frame-fullscreen)
 (show-paren-mode t)			;展示匹配括号
 (setq make-backup-files nil)		;不要生成备份文件
 (defalias 'yes-or-no-p 'y-or-n-p)	;y-or-n-p
@@ -20,13 +20,23 @@
 (global-unset-key (kbd "<right>"))
 (global-hl-line-mode t)
 (column-number-mode t)
-;; 设置没有激活的窗口mode-line为underline
+(global-set-key (kbd "M-s r") 'replace-regexp)
+
 (defun my-mode-line-modify ()
+  "设置没有激活的窗口mode-line为underline"
   (set-face-attribute 'mode-line-inactive nil
 		      :underline '(:color "DarkRed")
 		      :height 0.1
 		      :background (face-background 'default)))
 (add-hook 'after-init-hook #'my-mode-line-modify)
+
+(defun my-scratch ()
+  (interactive)
+  (if (get-buffer "*scratch*")
+      nil
+    (startup--get-buffer-create-scratch))
+  (switch-to-buffer "*scratch*"))
+(global-set-key (kbd "<f9>") 'my-scratch)
 
 (use-package exec-path-from-shell
   :ensure t
@@ -40,5 +50,12 @@
 
 (use-package all-the-icons
   :ensure t)
+
+(use-package auto-package-update
+   :ensure t
+   :config
+   (setq auto-package-update-delete-old-versions t
+         auto-package-update-interval 30)
+   (auto-package-update-maybe))
 
 (provide 'init-startup)
--- config/init-theme.el
+++ config/init-theme.el
@@ -3,10 +3,27 @@
   :config
   (setq doom-themes-enable-bold t
 	doom-themes-enable-italic t)
-  (load-theme 'doom-one t)
-  (doom-themes-visual-bell-config)
-  (doom-themes-org-config))
+  (load-theme 'doom-solarized-dark t)
+  ;; (doom-themes-visual-bell-config)
+  (doom-themes-org-config)
+  (setq doom-themes-treemacs-theme "doom-colors")
+  (doom-themes-treemacs-config))
 
-(set-frame-font "Monaco-13")
-(set-face-attribute 'default nil :height 130)
+(use-package cnfonts :ensure t :defer t)
+;; fonts
+(set-face-attribute
+ 'default nil
+ :font (font-spec :name "-APPL-Monaco-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1"
+                  :weight 'normal
+                  :slant 'normal
+                  :size 13.0))
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font
+   (frame-parameter nil 'font)
+   charset
+   (font-spec :name "-STON-幼圆-normal-normal-normal-*-*-*-*-*-d-0-iso10646-1"
+              :weight 'normal
+              :slant 'normal
+              :size 15.0)))
+
 (provide 'init-theme)
