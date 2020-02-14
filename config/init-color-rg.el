c721f03242caf9a6e760e9c80cfaef239338e67d
luhuaei <luhuaei@gmail.com>
2020-02-14 09:04:50 +0800

    fix color-rg custom error.

--- config/init-color-rg.el
+++ config/init-color-rg.el
@@ -1,9 +1,8 @@
 (use-package color-rg
   :ensure nil
   :quelpa (color-rg :fetcher github :repo "manateelazycat/color-rg")
-  :config
-  (define-key isearch-mode-map (kbd "M-s M-s") 'isearch-toggle-color-rg)
-  (global-set-key (kbd "M-s i") 'color-rg-search-input)
-  (global-set-key (kbd "M-s p") 'color-rg-search-project))
+  :bind (("M-s M-s" . 'isearch-toggle-color-rg)
+         ("M-s i" . 'color-rg-search-input)
+         ("M-s p" . 'color-rg-search-project)))
 
 (provide 'init-color-rg)
