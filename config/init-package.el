07b3eaf482eea84eb3ed16a976c01e5a2e2de901
luhuaei <luhuaei@gmail.com>
2020-02-14 09:05:55 +0800

    don't auto fetch github on init.

--- config/init-package.el
+++ config/init-package.el
@@ -2,6 +2,8 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package quelpa-use-package)
+(use-package quelpa-use-package
+  :config
+  (setq quelpa-update-melpa-p nil))
 
 (provide 'init-package)
