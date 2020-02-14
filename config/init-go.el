181430f26c80c868d93d0e45c274f2f15d1af72e
luhuaei <luhuaei@gmail.com>
2020-02-14 09:05:45 +0800

    use eglot

--- config/init-go.el
+++ config/init-go.el
@@ -1,8 +1,8 @@
 (use-package go-mode
-  :hook (go-mode . (lambda ()
-                     (add-hook 'before-save-hook 'gofmt-before-save)))
   :bind (:map go-mode-map
-              ("M-." . 'godef-jump))
-  )
+              ("M-." . 'godef-jump)
+              )
+  :config
+  (setq gofmt-command "goimports"))
 
 (provide 'init-go)
