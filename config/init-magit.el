edac252a132b6091b362871fd1e79a9a5f52043f
luhuaei <luhuaei@gmail.com>
2020-03-08 13:03:22 +0800

    magit use current windon display

--- config/init-magit.el
+++ config/init-magit.el
@@ -1,5 +1,6 @@
 (use-package magit
   :config
   (global-set-key (kbd "C-x g") 'magit-status)
-  (global-set-key (kbd "C-x M-g") 'magit-dispatch))
+  (global-set-key (kbd "C-x M-g") 'magit-dispatch)
+  (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1))
 (provide 'init-magit)
