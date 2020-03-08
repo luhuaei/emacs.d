8f78f1d2b37a999a0a564384c8660790e371bd2f
luhuaei <luhuaei@gmail.com>
2020-03-08 13:03:43 +0800

    refactor web mode

--- config/init-web.el
+++ config/init-web.el
@@ -1,6 +1,7 @@
 (use-package css-mode
-  :ensure nil
-  :init (setq css-indent-offset 2))
+  :init (setq css-indent-offset 2)
+  :mode "\\.\\(wxss\\)$"
+  )
 
 ;; SCSS mode
 (use-package scss-mode
@@ -10,93 +11,27 @@
 
 (use-package less-css-mode)
 
-;; CSS eldoc
-(use-package css-eldoc
-  :commands turn-on-css-eldoc
-  :hook ((css-mode scss-mode less-css-mode) . turn-on-css-eldoc))
-
 ;; JSON mode
 (use-package json-mode)
-
-;; JavaScript
-(use-package js2-mode
-  :defines flycheck-javascript-eslint-executable
-  :mode (("\\.js\\'" . js2-mode)
-         ("\\.jsx\\'" . js2-jsx-mode))
-  :interpreter (("node" . js2-mode)
-                ("node" . js2-jsx-mode))
-  :hook ((js2-mode . js2-imenu-extras-mode)
-         (js2-mode . js2-highlight-unused-variables-mode))
-  :config
-  (with-eval-after-load 'flycheck
-    (when (or (executable-find "eslint_d")
-              (executable-find "eslint")
-              (executable-find "jshint"))
-      (setq js2-mode-show-strict-warnings nil))
-    (when (executable-find "eslint_d")
-      ;; https://github.com/mantoni/eslint_d.js
-      ;; npm -i -g eslint_d
-      (setq flycheck-javascript-eslint-executable "eslint_d")))
-
-  (use-package js2-refactor
-    :diminish
-    :hook (js2-mode . js2-refactor-mode)
-    :config (js2r-add-keybindings-with-prefix "C-c C-m")))
-
-;; Live browser JavaScript, CSS, and HTML interaction
-(use-package skewer-mode
-  :diminish
-  :hook (((js-mode js2-mode). skewer-mode)
-         (css-mode . skewer-css-mode)
-         (web-mode . skewer-html-mode)
-         (html-mode . skewer-html-mode))
-  :init
-  ;; diminish
-  (with-eval-after-load 'skewer-css
-    (diminish 'skewer-css-mode))
-  (with-eval-after-load 'skewer-html
-    (diminish 'skewer-html-mode)))
 
 ;; Typescript
 (use-package typescript-mode)
 
-;; Run Mocha or Jasmine tests
-(use-package mocha
-  :config (use-package mocha-snippets))
-
-;; Major mode for CoffeeScript code
-(use-package coffee-mode
-  :config (setq coffee-tab-width 2))
-
 ;; Major mode for editing web templates
 (use-package web-mode
-  :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
+  :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\|wxml\\)$"
   :config
-  (setq web-mode-markup-indent-offset 4)
-  (setq web-mode-css-indent-offset 4)
-  (setq web-mode-code-indent-offset 4))
+  (setq web-mode-enable-auto-indentation nil)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-block-padding 0)
+  (setq web-mode-style-padding 1)
+  (setq web-mode-script-padding 0)
+  (setq web-mode-enable-current-element-highlight t)
+  (setq web-mode-enable-current-column-highlight t)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-css-colorization t)
+  (setq web-mode-enable-comment-interpolation t))
 
-;; Format HTML, CSS and JavaScript/JSON
-;; Install: npm -g install prettier
-(use-package prettier-js
-  :hook ((js-mode js2-mode json-mode web-mode css-mode sgml-mode html-mode)
-         .
-         prettier-js-mode))
-
-(use-package haml-mode)
-(use-package php-mode)
-
-;; REST
-(use-package restclient
-  :mode ("\\.http\\'" . restclient-mode)
-  :config
-  (use-package restclient-test
-    :ensure t
-    :diminish
-    :hook (restclient-mode . restclient-test-mode))
-
-  (with-eval-after-load 'company
-    (use-package company-restclient
-      :defines company-backends
-      :init (add-to-list 'company-backends 'company-restclient))))
 (provide 'init-web)
