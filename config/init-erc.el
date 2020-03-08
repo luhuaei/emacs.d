6faf50f99e86613d011dacd2b8e388fc0db7269e
luhuaei <luhuaei@gmail.com>
2020-03-08 13:02:06 +0800

    remove erc socks proxy

--- config/init-erc.el
+++ config/init-erc.el
@@ -1,9 +1,3 @@
-
-(use-package erc
-  :config
-  (require 'socks)
-  (setq socks-server (list "My erc scoks server" "127.0.0.1" 1080 5))
-  (setq socks-noproxy '("localhost"))
-  (setq erc-server-connect-function 'socks-open-network-stream))
+(use-package erc)
 
 (provide 'init-erc)
