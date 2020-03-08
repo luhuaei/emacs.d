453ffe2a9e1349d94978ce024653531a37c2d8ce
luhuaei <luhuaei@gmail.com>
2020-03-08 13:04:18 +0800

    eglot: disable flymake mode

--- config/init-lsp.el
+++ config/init-lsp.el
@@ -1,8 +1,6 @@
 (use-package eglot
-  :bind (:map eglot-mode-map
-              ("C-c h" . 'eglot-help-at-point))
   :config
-  (add-hook 'go-mode-hook #'eglot-ensure)
+  (add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
   (setq eglot-put-doc-in-help-buffer nil)
   (setq eldoc-message-function '(lambda (mes &rest args))) ;remove minibuffer help message
   (setq eglot-auto-display-help-buffer nil))
