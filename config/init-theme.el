96dffe8e6f1d042ae0445d02850e48a246cabaac
luhuaei <luhuaei@gmail.com>
2020-08-20 09:18:36 +0800

    remove doom themes

--- config/init-theme.el
+++ config/init-theme.el
@@ -1,12 +1,4 @@
-(use-package doom-themes
-  :config
-  (setq doom-themes-enable-bold t
-	doom-themes-enable-italic t)
-  (load-theme 'doom-gruvbox t)
-  (doom-themes-org-config)
-  (setq doom-themes-treemacs-theme "doom-colors")
-  (doom-themes-treemacs-config))
-
+(load-theme 'leuven t)
 (use-package cnfonts :defer t :disabled)
 
 (provide 'init-theme)
