187ffbb0e59653a9e8a49197163faca65a36268c
luhuaei <luhuaei@gmail.com>
2020-09-19 18:12:20 +0800

    git: add gutter mode

--- config/init-magit.el
+++ config/init-magit.el
@@ -3,4 +3,10 @@
   (global-set-key (kbd "C-x g") 'magit-status)
   (global-set-key (kbd "C-x M-g") 'magit-dispatch)
   (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package git-gutter
+  :config
+  (global-git-gutter-mode 't))
+
+
 (provide 'init-magit)
