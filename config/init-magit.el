ed2f38346d9b054425779ca5b42b8d20241acfc8
luhuaei <luhuaei@gmail.com>
2021-05-15 21:05:34 +0800

    remove useless key

--- config/init-magit.el
+++ config/init-magit.el
@@ -17,7 +17,6 @@
   :load-path emacs-extension-dir
   :config
   (global-set-key (kbd "C-x g") 'magit-status)
-  (global-set-key (kbd "C-x M-g") 'magit-dispatch)
   (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
 
   (add-hook 'magit-mode-hook #'magit-auto-revert-mode))
