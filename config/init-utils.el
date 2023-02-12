e01bba6f6acf75eab8993152f9b2e5cae8816fe6
luhuaei <luhuaei@gmail.com>
2023-02-12 18:12:38 +0800

    upgrade

--- config/init-utils.el
+++ config/init-utils.el
@@ -92,7 +92,7 @@
   :after (init)
   :config
   (auto-save-enable)
-  (setq auto-save-idle 1)
+  (setq auto-save-idle 5)
   (setq auto-save-silent t)
   (setq auto-save-delete-trailing-whitespace t))
 
